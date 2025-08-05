930beb86e99fe93e236809ebb22f7736b5c9fdcc
RdotF <tsarev02101977@gmail.com>
2025-08-05 14:16:31 +0300

    page-seeker TODO done

--- find-all-webpages/page-seeker.py
+++ find-all-webpages/page-seeker.py
@@ -1,26 +1,44 @@
 import requests
 from bs4 import BeautifulSoup
 #TO DO
-#user input
-#write results in file
-url = '' #e.g. https://www.youtube.com/shorts/
-domain = '' #e.g. https://www.youtube.com
-website = requests.get(url) #get the content
-website_text = website.text #content in unicode
+#user input - done
+#write results in file - done
+#try except - done
+def get_links(url):
+    links = []
+    try:
 
-soup_obj = BeautifulSoup(website_text) # creating Beautifulsoup object
+        website = requests.get(url)
+        website_text = website.text
+        soup = BeautifulSoup(website_text, features="lxml")
 
-links = []
+        for link in soup.find_all('a'):
+            links.append(link.get('href'))
+        links.sort()
 
-for link in soup_obj.find_all('a'):
-    full_url = domain + str(link)
-    if requests.request('GET', full_url) == 200 or requests.request('GET', link) == 200 or requests.request('GET', domain + str(link)) == 200:
-        links.append(link.get('href'))
+    except Exception as e:
+        print(f'Something went wrong: {e}')
+    return links
+def put_links_in_file(links, url):
+    try:
+        with open('nestedLinks.txt', 'w') as file:
+            file.write(url+'\n')
+            for i in range(len(links)):
+                file.write(f'{links[i]}\n')
+        file.close()
 
-links.sort()
-links = set(links)
+    except Exception as e:
+        print(f'Something went wrong: {e}')
+    return 1
 
-for link in links:
-    print(link)
 
-print('total quantity:', len(links))
+url = input('Enter your url (e.g. https://www.youtube.com/):\n')
+nested_links = list(set(get_links(url))) #get rid of doubles
+
+print(f'Total of the links found: {len(nested_links)}')
+print('Putting the links in file...')
+put_links_in_file(links=nested_links, url=url)
+
+
+
+
