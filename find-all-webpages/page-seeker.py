ac713d69475d5bf8f3d3355a4c452f29b7677e1d
RdotF <tsarev02101977@gmail.com>
2025-08-05 14:59:56 +0300

    page-seeker TODO 2 done

--- find-all-webpages/page-seeker.py
+++ find-all-webpages/page-seeker.py
@@ -1,9 +1,8 @@
 import requests
 from bs4 import BeautifulSoup
 #TO DO
-#user input - done
-#write results in file - done
-#try except - done
+#filter links - done
+#check url - done
 def get_links(url):
     links = []
     try:
@@ -22,7 +21,7 @@ def get_links(url):
 def put_links_in_file(links, url):
     try:
         with open('nestedLinks.txt', 'w') as file:
-            file.write(url+'\n')
+            file.write('ROOTLINK: ' + url+'\n')
             for i in range(len(links)):
                 file.write(f'{links[i]}\n')
         file.close()
@@ -30,15 +29,41 @@ def put_links_in_file(links, url):
     except Exception as e:
         print(f'Something went wrong: {e}')
     return 1
+def filter_nested_links(links):
+    filtered_links = []
+    if isinstance(links, list):
+        for i in links:
+            if '/' in i and len(i) > 1:
+                filtered_links.append(i)
+        return filtered_links
+    else:
+        print('wrong parameter got into filtered_nested_links function. Should be a list')
+
+
+
+def check_url(url):
+    if 'http://' not in url and 'https://' not in url or len(url) <= 8:
+        url = 0 #false
+    elif not(url[-1] == '/'):
+        return url+'/'
+    if url == 0:
+        print('Wrong link.')
+        print('Example of valid link: https://www.youtube.com/')
+        print('Exiting the program')
+        exit()
+    else:
+        return url
 
 
 url = input('Enter your url (e.g. https://www.youtube.com/):\n')
+url = check_url(url)
+
 nested_links = list(set(get_links(url))) #get rid of doubles
-
-print(f'Total of the links found: {len(nested_links)}')
+filtered_links = filter_nested_links(nested_links)
+print(f'Total unique links found: {len(filtered_links)}')
 print('Putting the links in file...')
-put_links_in_file(links=nested_links, url=url)
-
+if put_links_in_file(links=filtered_links, url=url):
+    print('The links were put in the file without an issue.')
 
 
 
